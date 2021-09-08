5764d188c689c9007e92c28ccbf924295760c07a
Vivek Alamuri <vivek.alamuri@gmail.com>
2021-09-07 20:33:46 -0400

    basic image preview working

--- blood-detect/src/components/dashboard/dashboard.jsx
+++ blood-detect/src/components/dashboard/dashboard.jsx
@@ -1,31 +1,53 @@
-import React, {useState} from 'react'
-import { useAuth } from  "../../contexts/AuthContext"
-import { Button } from 'react-bootstrap'
-import { Link, useHistory } from 'react-router-dom'
-import { Navbar } from '../navbar/navbar'
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { Button } from "react-bootstrap";
+import { Navbar } from "../navbar/navbar";
+import axios from "axios";
 
 export const Dashboard = () => {
-    const { currentUser, logout } = useAuth()
-    const [error, setError] = useState("")
-    const history = useHistory()
-    const handleLogout = async () => {
-        try {
-            await logout()
-            history.push('/signin')
-
-        } catch {
-            setError('log out fail')
+  const [file, setFile] = useState(null);
+  const [img, setImg] = useState("https://cdn.pixabay.com/photo/2017/07/08/11/10/dot-pattern-2484077_960_720.jpg");
+  
+  const handleFileUpload = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+        if (reader.readyState === 2) {
+            setImg(reader.result)
         }
     }
-    return (
-        <>
-            <Navbar />
-           dashboard
-           <h3>
-           {currentUser.email}
-           </h3>
-           <Button variant="link" onClick={handleLogout}>Log out</Button>
-           {error}
-        </>
-    )
-}
+    reader.readAsDataURL(e.target.files[0])
+  };
+
+  useEffect(() => {
+    console.log("dashboard loaded");
+    
+  }, [img])
+
+  const handleFileSelect = (e) => {
+    console.log(e.target.files[0]);
+  };
+
+  const { currentUser } = useAuth();
+  return (
+    <>
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <h3>{currentUser.email}</h3>
+      <img src={img} alt="" className="src" height="100" width="100"/>
+      <input
+        type="file"
+        accept="image/*"
+        id="input"
+        style={{ opacity: 0.1 }}
+        onChange={handleFileUpload}
+      />
+      <div className="label">
+        <label htmlFor="input">Select Image</label>
+      </div>
+      <Button onClick={handleFileUpload}>Upload Image</Button>
+      {/* <Button variant="link" onClick={handleSubmit}>Log out</Button> */}
+    </>
+  );
+};
