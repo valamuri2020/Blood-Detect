00a41e318a1c07412eaea5886e2ab91ebf65581b
Vivek Alamuri <vivek.alamuri@gmail.com>
2021-09-09 21:04:45 -0400

    cloud function set up

--- blood-detect/functions/index.js
+++ blood-detect/functions/index.js
@@ -4,6 +4,11 @@ const functions = require("firebase-functions");
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
+//   functions.logger.info("Hello logs!", { structuredData: true });
 //   response.send("Hello from Firebase!");
 // });
+
+exports.onFileChange = functions.storage.object().onFinalize((event) => {
+  console.log(event);
+  return;
+});
